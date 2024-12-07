9a3fe60c545461287d82ec287496865faeed28f1
ridnA314 <ridnA776@gmail.com>
2024-12-07 15:47:32 +0900

    update recommendation

--- server/app.py
+++ server/app.py
@@ -1,93 +1,166 @@
 from flask import Flask, request, jsonify, make_response
 from riot_api import getUserAccount, getMatchIDByPuuid, getMatchByMatchId
 import csv
+from random import shuffle
 
-app = Flask(__name__) 
+import time
 
-def getUserFeature(match_info_list):
-    mean_and_std_file = open("MeanAndStd.csv", "r")
-    reader = csv.reader(mean_and_std_file)
+app = Flask(__name__)
 
-    header = next(reader)
-    mean_data = next(reader)
-    std_data = next(reader)
-    
-    count = len(match_info_list)
+with open("ChampionLineAverages_user.csv", mode="r", encoding="utf-8") as file:
+    CHAMPION_LINE_DATA = [row for row in csv.reader(file)]
+
+with open("championDivision.csv", mode="r", encoding="utf-8") as file:
+    CHAMPION_DIVISION_DATA = [row for row in csv.reader(file)]
+
+with open("championByRole.csv", mode="r", encoding="utf-8") as file:
+    CHAMPION_ROLE_DATA = [row for row in csv.reader(file)]
+
+with open("beginner_recommend.txt", mode="r", encoding="utf-8") as file:
+    BEGINNER_RECOMMEND_DATA = [line.strip() for line in file]
+
+def getMeanAndVar(most_champion, most_line):
+    for row in CHAMPION_LINE_DATA[1:]:
+        if row[0].lower() == most_champion and row[1] == most_line:
+            return row[2:7], row[7:12]
+    return -1, -1
+
+def getUserFeature(match_info_list, mean_data, std_data):
     user_feature_data = {
         "aggressive" : 0, #killsNearEnemyTurret/kills
         "defensive" : 0, #killsUnderOwnTurret/kills
 
-        "split" : 0, #damageDealtToBuildings/totalDamageDealt
         "teamfight" : 0, #totalDamageDealtToChampions/totalDamageDealt
+        "split" : 0, #damageDealtToBuildings/totalDamageDealt
 
         "roamness" : 0 #soloKills/kills
     }
-    for i in range(count):
-        participants = match_info_list[i]["participants"]
-        challenges = participants["challenges"]
 
-        kills = participants["kills"]
-        totalDamageDealt = participants["totalDamageDealt"]
+    for participant in match_info_list:
+        challenges = participant["challenges"]
 
-        if kills != 0 and kills != None:
-            user_feature_data["aggressive"] += challenges["killsNearEnemyTurret"] / kills
-            user_feature_data["defensive"] += challenges["killsUnderOwnTurret"] / kills
-            user_feature_data["roamness"] += challenges["soloKills"] / kills
+        kills = participant.get("kills", 0)
+        totalDamageDealt = participant.get("totalDamageDealt", 0)
+
+        if kills:
+            user_feature_data["aggressive"] += challenges.get("killsNearEnemyTurret", 0) / kills
+            user_feature_data["defensive"] += challenges.get("killsUnderOwnTurret", 0) / kills
+            user_feature_data["roamness"] += challenges.get("soloKills", 0) / kills
         
-        if totalDamageDealt != 0 and totalDamageDealt != None:
-            user_feature_data["split"] += participants["damageDealtToBuildings"] / totalDamageDealt
-            user_feature_data["teamfight"] += participants["totalDamageDealtToChampions"] / totalDamageDealt
+        if totalDamageDealt:
+            user_feature_data["split"] += participant.get("damageDealtToBuildings", 0) / totalDamageDealt
+            user_feature_data["teamfight"] += participant.get("totalDamageDealtToChampions", 0) / totalDamageDealt
     
-    user_feature_data["aggressive"] = user_feature_data["aggressive"] / count
-    user_feature_data["defensive"] = user_feature_data["defensive"] / count
-    user_feature_data["split"] = user_feature_data["split"] / count
-    user_feature_data["teamfight"] = user_feature_data["teamfight"] / count
-    user_feature_data["roamness"] = user_feature_data["roamness"] / count
+    count = len(match_info_list)
+    for index, (key, value) in enumerate(user_feature_data.items()):
+        user_feature_data[key] = (value / count - float(mean_data[index])) / float(std_data[index])
 
-    user_feature_data["aggressive"] = (user_feature_data["aggressive"] - mean_data[0]) / std_data[0]
-    user_feature_data["defensive"] = (user_feature_data["defensive"] - mean_data[1]) / std_data[1]
-    user_feature_data["split"] = (user_feature_data["split"] - mean_data[2]) / std_data[2]
-    user_feature_data["teamfight"] = (user_feature_data["teamfight"] - mean_data[3]) / std_data[3]
-    user_feature_data["roamness"] = (user_feature_data["roamness"] - mean_data[4]) / std_data[4]
-    
+    return normalizeFeature(user_feature_data)
+
+def normalizeFeature(features):
     #기준점으로 세구간으로 나누기
-    for key, value in user_feature_data.items():
-        if value > 1:
-            user_feature_data[value] = 1
-        elif value < -1:
-            user_feature_data[value] = -1
+    bounds = {"upper": [0.26, 0.1, 0.22, 0.2, 0.09], "lower": [-0.01, -0.09, -0.24, -0.21, -0.29]}
+    for index, key in enumerate(features):
+        feature = features[key]
+        if feature >= bounds["upper"][index]:
+            features[key] = 1
+        elif feature <= bounds["lower"][index]:
+            features[key] = -1
         else:
-            user_feature_data[value] = 0
+            features[key] = 0
     
-    user_feature_data["aggressive"] -= user_feature_data["defensive"]
-    user_feature_data["split"] -= user_feature_data["teamfight"]
-    user_feature_data["roamness"] *= -1
-    del user_feature_data['defensive']
-    del user_feature_data['teamfight']
+    #[-2, -1, 0, 1, 2] -> [-1, 0, 1]
+    aggressive = features["aggressive"] - features["defensive"]
+    if aggressive >= 1:
+        aggressive = 1
+    elif aggressive <= -1:
+        aggressive = -1
+    else:
+        aggressive = 0
+    features["aggressive"] = aggressive
+    
+    split = features["split"] - features["teamfight"]
+    if split >= 1:
+        split = 1
+    elif split <= -1:
+        split = -1
+    else:
+        split = 0
+    features["split"] = split
 
-    mean_and_std_file.close()
-    return user_feature_data
+    features["roamness"] *= -1
+    return features
 
-def getCharacterAndChampion(match_info_list):
-    champion_cluster_file = open("championCluster.csv", "r")
-    reader = csv.reader(champion_cluster_file)
-
-    user_feature = getUserFeature(match_info_list)
-    champion_name = ''
-    score = 0
-
-    #정해진 기준대로 유저 feature와 챔피언별 feature를 비교하여 적합한 챔피언 찾기
-
+def getCharacterAndChampion(match_info_list, most_champion, most_line, max_recommend):
+    mean, variance = getMeanAndVar(most_champion, most_line)
+    if mean == -1:
+        return -1, [], []
+    
+    user_feature = getUserFeature(match_info_list, mean, variance)
     characteristics = {
         'aggression': user_feature["aggressive"],
         'roamness': user_feature["roamness"],
         'backdoor': user_feature["split"],
     }
 
-    return characteristics, champion_name, score
+    recommend_champion_list = []
+    line_list = []
+    for row in CHAMPION_DIVISION_DATA[1:]:
+        if (int(row[2]) == characteristics["aggression"] and
+            int(row[3]) == characteristics["backdoor"] and
+            int(row[4]) == characteristics["roamness"]):
+            recommend_champion_list.append(row[0])
+            line_list.append(row[1])
+        if len(recommend_champion_list) >= max_recommend:#최대 추천수 까지만
+            break
+    return characteristics, recommend_champion_list, line_list
+
+def getChampionByRole(most_champion, most_line, max_recommend):
+    recommend_champion_list = []
+    line_list = []
+
+    role_champion_list = []
+
+    index = 0
+    change_point = 0
+    pre_role = '0'
+    target_role = '0'
+    for row in CHAMPION_ROLE_DATA[1:]:
+        champion_name = row[0]
+        role = row[2]
+        if pre_role != role:
+            pre_role = role
+            change_point = index
+        if most_champion == champion_name:
+            target_role = role
+            break
+        index += 1
+    for row in reader:#조건문 뺴고 이어서
+        if target_role != row[2]:
+            break
+        index += 1
+    role_champion_list = CHAMPION_ROLE_DATA[change_point+1:index]
+
+    for element in role_champion_list:
+        recommend_champion_list.append(element[0])
+        line_list.append(element[1])
+        if len(recommend_champion_list) >= max_recommend:#최대 추천수 까지만
+            break
+    
+    return recommend_champion_list, line_list
+
+def getBeginnerChampion(max_recommend):
+    champion_list = BEGINNER_RECOMMEND_DATA[::2]
+    line_list = BEGINNER_RECOMMEND_DATA[1::2]
+
+    random_list = list(zip(champion_list, line_list))
+    shuffle(random_list)
+    return zip(*random_list[:max_recommend])
 
 @app.route('/api/analyze', methods=['GET'])
 def getRecommandInfo():
+    max_recommend = 20
+
     data = {
         'characteristics':{
             'aggression': -1, #공격성 : 1, 0, -1 (세분류 : 높음, 중간, 낮음)
@@ -95,52 +168,59 @@ def getRecommandInfo():
             'backdoor': -1, #백도어? : 1, 0, -1
         },
         'recommends':{
-            'champion_name': '',
-            'score' : 0
+            'champion_list': [],
+            'line_list': []
         }
     }
+
     region = request.args.get('region')
     nickname = request.args.get('nickname')
-
-    line = request.args.get('line')
     
     split_list = nickname.split('#')
     if len(split_list) != 2:
-        response = make_response(jsonify(data))
-        response.status_code = 400
-        return response
+        print('잘못된 요청')
+        return make_response(jsonify(data), 400)
     
-    puuid = getUserAccount(region, split_list[0], split_list[1])
+    puuid = getUserAccount(region.lower(), split_list[0], split_list[1])
     if puuid == -1:
-        response = make_response(jsonify(data))
-        response.status_code = 404
-        return response
+        print("지역 : " + region + "닉네임1 : " + split_list[0] + "닉네임2 : " + split_list[1])
+        print('유저를 찾지 못함')
+        return make_response(jsonify(data), 404)
     
     match_id_list = getMatchIDByPuuid(region, puuid)
     if match_id_list == -1:
-        response = make_response(jsonify(data))
-        response.status_code = 404
-        return response
+        print('경기기록 요청에 문제 발생')
+        return make_response(jsonify(data), 404)
     elif match_id_list == 0:
-        #data에 초보자 추천 챔피언을 추가
-        response = make_response(jsonify(data))
-        response.status_code = 200
-        return response
+        champion_list, line_list = getBeginnerChampion(max_recommend)
+        data["recommends"].update({"champion_list": champion_list, "line_list": line_list})
+        print('경기기록이 50경기를 못넘음, 초보자 추천 루트')
+        return make_response(jsonify(data), 200)
     
-    match_info_list = getMatchByMatchId(region, match_id_list)
+    match_info_list, most_champion, most_line = getMatchByMatchId(region, puuid, match_id_list)
     if match_info_list == -1:
-        response = make_response(jsonify(data))
-        response.status_code = 404
-        return response
+        champion_list, line_list = getBeginnerChampion(max_recommend)
+        data["recommends"].update({"champion_list": champion_list, "line_list": line_list})
+        print('많이 플레이한 챔피언-라인 기록이 기준치를 못넘김, 초보자 추천 루트')
+        return make_response(jsonify(data), 200)
     
-    characteristics, champion_name, score = getCharacterAndChampion(match_info_list)
+    characteristics, champion_list, line_list = getCharacterAndChampion(match_info_list, most_champion, most_line, max_recommend)
+    if len(champion_list) < 1:
+        if characteristics != -1:
+            data['characteristics'] = characteristics
+        champion_list, line_list = getChampionByRole(most_champion, most_line, max_recommend)
+        if len(champion_list) < 1:
+            print('역할군 추천도 불가')
+            return make_response(jsonify(data), 404)
+        
+        data["recommends"].update({"champion_list": champion_list, "line_list": line_list})
+        print('성향에 맞는 챔피언이 없음, 역할군 기반 추천 루트')
+        return make_response(jsonify(data), 200)
+    
     data['characteristics'] = characteristics
-    data['recommends']['champion_name'] = champion_name
-    data['recommends']['score'] = score
-
-    response = make_response(jsonify(data))
-    response.status_code = 200
-    return response
+    data["recommends"].update({"champion_list": champion_list, "line_list": line_list})
+    print('성향에 맞는 챔피언을 찾음!')
+    return make_response(jsonify(data), 200)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8090)
--- server/riot_api.py
+++ server/riot_api.py
@@ -1,10 +1,10 @@
 import requests
 import time
 
-api_key = "key"
+api_key = "RGAPI-7c91809e-7d18-4a43-b3ff-fe578746a4b5"
 header = {"X-Riot-Token" : api_key}
-request_time = 0
-numbering = 1
+
+API_LIMIT_TIME = 0.05
 
 def getUserAccount(region, summoner_name, tag_line):
     base = f"https://{region}.api.riotgames.com"
@@ -12,8 +12,8 @@ def getUserAccount(region, summoner_name, tag_line):
     url = base + end_point
 
     response = requests.get(url, headers=header)
-
-    time.sleep(0.05)
+    time.sleep(API_LIMIT_TIME)
+    
     if response.status_code == 200:
         data = response.json()
         return data["puuid"]
@@ -23,39 +23,57 @@ def getUserAccount(region, summoner_name, tag_line):
 
 def getMatchIDByPuuid(region, puuid):
     base = f"https://{region}.api.riotgames.com"
-    end_point = f"/lol/match/v5/matches/by-puuid/{puuid}/ids"
+    end_point = f"/lol/match/v5/matches/by-puuid/{puuid}/ids?count=98"
     url = base + end_point
 
     response = requests.get(url, headers=header)
+    time.sleep(API_LIMIT_TIME)
 
-    time.sleep(0.05)
     if response.status_code == 200:
         data = response.json()
-        if len(data) < 10:
+        if len(data) < 50:
             return 0
         return data
     else:
         print("Request failed with status code:", response.status_code)
         return -1
 
-def getMatchByMatchId(region, match_id_list):
+def getMatchByMatchId(region, puuid, match_id_list):
     match_list = []
+    temp_list = []
+    champion_line_count = {}
+
+    start = time.time()
     base = f"https://{region}.api.riotgames.com"
     for i in range(len(match_id_list)):
         match_id = match_id_list[i]
         end_point = f"/lol/match/v5/matches/{match_id}"
         url = base + end_point
+
         response = requests.get(url, headers=header)
-        
         if response.status_code == 200:
             data = response.json()
-            match_list.append(data)
+            for participant in data["info"]["participants"]:#경기당 10명의 플레이어
+                if participant["puuid"] == puuid:
+                    champion_name = participant["championName"].lower()
+                    teamPosition = participant["teamPosition"]
+                    key = champion_name + '-' + teamPosition
+
+                    #챔피언과 라인을 묶어서 카운팅
+                    champion_line_count[key] = champion_line_count.get(key, 0) + 1
+                    temp_list.append(participant)
         else:
             print("Request failed with status code:", response.status_code)
-        time.sleep(0.05)
+        time.sleep(API_LIMIT_TIME)
     
-    if len(match_list) < 10:
-        #10이하는 미리 걸렀으니 10이상이 안나오면 데이터를 못찾은거
-        return -1
+    most_key = max(champion_line_count, key=champion_line_count.get)
+    result = most_key.split('-')
+    match_list = [m for m in temp_list if m["championName"].lower() == result[0] and m["teamPosition"] == result[1]]
+    
+    end = time.time()
+    print(f"get match data time : {end - start:.5f} sec")
+
+    if len(match_list) < 5:
+        return -1, -1, -1
     else:
-        return match_list
+        return match_list, result[0], result[1]
